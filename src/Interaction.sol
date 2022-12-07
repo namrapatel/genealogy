8b1691c19443cc873425fd954092e91d9b44773b
Namra Patel <namrajpatel@gmail.com>
2022-12-07 17:54:58 -0500

    buggy, pre-review

--- src/Interaction.sol
+++ src/Interaction.sol
@@ -10,30 +10,62 @@ struct Role {
     uint256 entity;
 }
 
+struct Turn {
+    address state;
+    uint256[] entitiesWithPermission;
+    uint256 totalCallsAllowed;
+    mapping(uint256 => uint256) entityToCallsAllowed;
+    uint8[] roleIndexes;
+    uint256[] sequenceOfEntitiesTakingTurns;
+}
+
 abstract contract Interaction {
    
     uint256 turnCounter;
     Role[] roles;
-    // Turn Number -> State
-    mapping(uint256 => address) turnToState;
-    // Turn Number -> Entities with permission to execute this interaction
-    mapping(uint256 => uint256[]) turnToEntitiesWithPermission;
-    // Turn Number -> Total Calls Allowed
-    mapping(uint256 => uint256) turnToTotalCallsAllowed;
-    // Turn Number -> Entity -> Calls Allowed for that entity
-    mapping(uint256 => mapping(uint256 => uint256)) turnToEntityToCallsAllowed;
+    mapping(uint256 => Turn) turns;
+
+    bool isSequenced;
+    uint256 currentSequenceIndex;
     // Turn Number -> Sequence of Entites taking turns
     mapping(uint256 => uint256[]) turnToSequenceOfEntitiesTakingTurns;
-    mapping(uint256 => uint8[]) turnToRoleIndexes; 
 
-    // There needs to be a mapping between State Procedure
 
-    constructor() {
+    constructor(
+        Role[] memory _roles,
+        address[] memory _states,
+        uint256[] memory _totalCallsAllowed,    
+        uint256[][] memory _entitiesWithPermission,
+        uint256[][] memory _entityToCallsAllowed,
+        uint8[][] memory _roleIndexes,
+        bool _isSequenced,
+        uint256[] memory _sequenceOfEntitiesTakingTurns,
+        uint256 _currentSequenceIndex
+    ) {
         turnCounter = 0;
+        roles = _roles;
+        isSequenced = _isSequenced;
+        currentSequenceIndex = _currentSequenceIndex;
+
+        for(uint256 i = 0; i < _states.length; i++) {
+            turns[i] = Turn({
+                state: _states[i],
+                entitiesWithPermission: _entitiesWithPermission[i],
+                totalCallsAllowed: _totalCallsAllowed[i],
+                roleIndexes: _roleIndexes[i],
+                sequenceOfEntitiesTakingTurns: _sequenceOfEntitiesTakingTurns
+            });
+
+            for(uint256 j = 0; j < _entitiesWithPermission[i].length; j++) {
+                turns[i].entityToCallsAllowed[_entitiesWithPermission[i][j]] = _entityToCallsAllowed[i][j];
+            }
+        }
     }
 
     function authenticateAndExecute() public {
-        uint256[] memory entitiesWithPermission = turnToEntitiesWithPermission[turnCounter];
+        Turn memory turn = turns[turnCounter];
+
+        uint256[] memory entitiesWithPermission = turns[turnCounter].turnToEntitiesWithPermission;
         uint256 senderEntity = addressToEntity(msg.sender);
 
         // Require that senderEntity is in the entitiesWithPermission array
@@ -47,21 +79,21 @@ abstract contract Interaction {
         require(senderHasPermission, "Sender does not have permission to execute this interaction");
 
         // Require that the sender has not exceeded their calls allowed
-        uint256 callsAllowed = turnToEntityToCallsAllowed[turnCounter][senderEntity];
+        uint256 callsAllowed = turns[turnCounter][senderEntity].turnToEntityToCallsAllowed;
         require(callsAllowed > 0, "Sender has exceeded their calls allowed");
 
         // Require that the total calls allowed has not been exceeded
-        uint256 totalCallsAllowed = turnToTotalCallsAllowed[turnCounter];
+        uint256 totalCallsAllowed = turns[turnCounter].turnToTotalCallsAllowed;
         require(totalCallsAllowed > 0, "Total calls allowed has been exceeded");
 
         // Decrement the total calls allowed and the sender's calls allowed
-        turnToTotalCallsAllowed[turnCounter]--;
-        turnToEntityToCallsAllowed[turnCounter][senderEntity]--;
+        turns[turnCounter].turnToTotalCallsAllowed--;
+        turns[turnCounter][senderEntity].turnToEntityToCallsAllowed--;
 
         // Get the role indexes for the sender
-        uint8[] memory roleIndexes = turnToRoleIndexes[turnCounter];
+        uint8[] memory roleIndexes = turns[turnCounter].turnToRoleIndexes;
 
-        execute(turnToState[turnCounter], roleIndexes);
+        execute(turns[turnCounter].turnToState, roleIndexes);
     }
 
     function execute(address stateAddress, uint8[] memory roleIndexes) internal {
@@ -81,7 +113,9 @@ abstract contract Interaction {
         changeState(executionResult);
     }
 
-    function changeTurn(bytes memory executionResult) internal virtual;
+    function changeTurn(bytes memory executionResult) internal virtual {
+        
+    }
 
     function changeState(bytes memory executionResult) internal virtual;
 
